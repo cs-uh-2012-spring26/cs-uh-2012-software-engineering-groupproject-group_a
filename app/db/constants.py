7f27c306415041e7d0fd2ddc86be014fb126fa01
Nadja Kaludjerovic <nk3658@nyu.edu>
2026-04-25 17:10:05 +0200

    refactor: improve modularity, abstraction, and service layer separation

--- app/db/constants.py
+++ app/db/constants.py
@@ -1,2 +1,13 @@
 # Generic fields
 ID = "_id"
+
+CLASS_WINDOW_DAYS = 14
+CLASS_COLLECTION = "classes"
+
+class_name = "class_name"
+start_time = "start_time"
+end_time = "end_time"
+location = "location"
+capacity = "capacity"
+trainer_name = "trainer_name"
+remaining_spots = "remaining_spots"
